41f97dc02ffa30b221c575eb155d7c3a32e867a4
Chan Lee <chanethanlee@gmail.com>
2018-11-28 23:54:19 -0800

    solve permutation check with hashmap, O(n).

--- src/StringAndArray.java
+++ src/StringAndArray.java
@@ -4,6 +4,7 @@ public class StringAndArray {
     public static void main (String args[]) {
 //        System.out.println(CheckPermutation("god","fog"));
 //        System.out.println(CheckPermutation("god","dog"));
+        System.out.println(CheckPermutationHashMap("dog","god"));
     }
 
     public static boolean CheckPermutation(String str1, String str2) {
@@ -20,4 +21,24 @@ public class StringAndArray {
         }
         return false;
     }
+
+    public static boolean CheckPermutationHashMap(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+        int len = str1.length();
+        int[] letters = new int[128];
+        for (int i=0; i< len; i++) {
+            letters[str1.charAt(i)]++;
+        }
+        for (int i=0; i< len; i++) {
+            letters[str2.charAt(i)]--;
+            if (letters[str2.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // letters[str1.charAt(i)], str1.char seems to be coerced as int.
+    // int[128] was due to ASCII code, a starting around 95 to +26, 121.
+    // As array doubles the size, 128 seems like effective minimum.
 }
